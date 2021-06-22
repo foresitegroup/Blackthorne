9e8d4ddc032bc4ee0ab799827f2864bb8acf4286
marklippert <lippert@gmail.com>
2021-06-22 11:07:58 -0500

    Replace map

--- contact-us.php
+++ contact-us.php
@@ -26,7 +26,7 @@ include "header.php";
 <div style="clear: both;"></div><br>
 <br>
 
-<!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBg090YmprLM-yHNNxAwJEwaCn_4v67HnA"></script> -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBg090YmprLM-yHNNxAwJEwaCn_4v67HnA"></script>
 <script>
   // function ViewLargerMap(VLMa, map) {
   //   var VLMui = document.createElement('a');
@@ -38,53 +38,53 @@ include "header.php";
   //   VLMa.appendChild(VLMui);
   // }
 
-  // function initialize() {
-  //   var MyLatLng = new google.maps.LatLng(43.0595565,-88.4053932);
-  //   var mapCanvas = document.getElementById('map-canvas');
-  //   var mapOptions = {
-  //     center: MyLatLng,
-  //     zoom: 16,
-  //     disableDefaultUI: true,
-  //     zoomControl: true,
-  //     zoomControlOptions: {
-  //       style: google.maps.ZoomControlStyle.SMALL,
-  //       position: google.maps.ControlPosition.RIGHT_BOTTOM
-  //     },
-  //     mapTypeId: google.maps.MapTypeId.ROADMAP
-  //   }
+  function initialize() {
+    var MyLatLng = new google.maps.LatLng(43.0595565,-88.6000000);
+    var mapCanvas = document.getElementById('map-canvas');
+    var mapOptions = {
+      center: MyLatLng,
+      zoom: 9,
+      disableDefaultUI: true,
+      zoomControl: true,
+      zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.SMALL,
+        position: google.maps.ControlPosition.RIGHT_BOTTOM
+      },
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
 
-  //   var map = new google.maps.Map(mapCanvas, mapOptions)
-  //   map.set('styles', [
-  //     {
-  //       stylers: [
-  //         { "saturation": -100 },
-  //         { "visibility": "simplified" }
-  //       ]
-  //     }
-  //   ]);
+    var map = new google.maps.Map(mapCanvas, mapOptions)
+    map.set('styles', [
+      {
+        stylers: [
+          { "saturation": -100 },
+          { "visibility": "simplified" }
+        ]
+      }
+    ]);
 
-  //   var marker = new google.maps.Marker({
-  //     position: MyLatLng,
-  //     map: map
-  //   });
+    // var marker = new google.maps.Marker({
+    //   position: MyLatLng,
+    //   map: map
+    // });
 
-  //   var infowindow = new google.maps.InfoWindow({
-  //     content: '<div id="content"><div id="bodyContent"><strong>Blackthorne Partners</strong><br>514 Wells Street<br>Suite 2B<br>Delafield, WI 53018<br><a href="https://www.google.com/maps/place/Blackthorne+Partners/@43.0595565,-88.4053932,15z/data=!4m5!3m4!1s0x0:0x994eadaba5ea4f34!8m2!3d43.0595565!4d-88.4053932" target="new">View larger map</a></div></div>'
-  //   });
+    // var infowindow = new google.maps.InfoWindow({
+    //   content: '<div id="content"><div id="bodyContent"><strong>Blackthorne Partners</strong><br>514 Wells Street<br>Suite 2B<br>Delafield, WI 53018<br><a href="https://www.google.com/maps/place/Blackthorne+Partners/@43.0595565,-88.4053932,15z/data=!4m5!3m4!1s0x0:0x994eadaba5ea4f34!8m2!3d43.0595565!4d-88.4053932" target="new">View larger map</a></div></div>'
+    // });
 
-  //   google.maps.event.addListener(marker, 'click', function() {
-  //     infowindow.open(map,marker);
-  //   });
+    // google.maps.event.addListener(marker, 'click', function() {
+    //   infowindow.open(map,marker);
+    // });
 
-  //   var vlmDiv = document.createElement('div');
-  //   var vlm = new ViewLargerMap(vlmDiv, map);
-  //   vlmDiv.index = 1;
-  //   map.controls[google.maps.ControlPosition.TOP_LEFT].push(vlmDiv);
-  // }
+    var vlmDiv = document.createElement('div');
+    var vlm = new ViewLargerMap(vlmDiv, map);
+    vlmDiv.index = 1;
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(vlmDiv);
+  }
 
-  // google.maps.event.addDomListener(window, 'load', initialize);
+  google.maps.event.addDomListener(window, 'load', initialize);
 </script>
 
-<!-- <div id="map-canvas"></div> -->
+<div id="map-canvas"></div>
 
 <?php include "footer.php"; ?>
