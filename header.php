e5ddd0f6ba623cf976de55feb88430df08fd4764
foresitegroup <mark@foresitegrp.com>
2022-06-07 10:09:52 -0500

    Add new analytics code

--- header.php
+++ header.php
@@ -40,7 +40,7 @@ function email($address, $name="") {
     <script type="text/javascript">DD_belatedPNG.fix('img, .png');</script>
     <![endif]-->
     
-    <!-- BEGIN Google Analytics -->
+    <!-- BEGIN Google Analytics [This will stop working 7/1/23] -->
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -51,6 +51,15 @@ function email($address, $name="") {
       ga('send', 'pageview');
     </script>
     <!-- END Google Analytics -->
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-RLCRJQV5LJ"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-RLCRJQV5LJ');
+    </script>
   </head>
   <body>
     
